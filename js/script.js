73c20cf98d0635aca05b77ec51d8301087e762b9
sebanek96 <116579697+sebanek96@users.noreply.github.com>
2023-01-11 17:41:11 +0100

    js changed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,47 @@
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
-let wantGetElement = document.querySelector(".js-wantGet");
-let amountElement = document.querySelector(".js-amount");
-
-document.addEventListener("submit", (event) => {
-    event.preventDefault();
+{
+    const welcome = () => {
+        console.log('Welcome to my currency converter')
+    }
 
 
-    let amount = amountElement.value;
-    let wantGet = wantGetElement.value;
+    const resultElement = document.querySelector(".js-result");
+    const currencyElement = document.querySelector(".js-currency");
+    const amountElement = document.querySelector(".js-amount");
+    const formElement = document.querySelector(".js-form");
 
-    let result = amount / wantGet;
+    const rateEUR = 4.69;
+    const rateUSD = 4.36;
+    const rateGBP = 5.28;
 
-    resultElement.innerText = result.toFixed(2);
-});
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        let result;
+
+        switch (currency) {
+            case "EUR":
+                result = amount / rateEUR;
+                break;
+
+            case "USD":
+                result = amount / rateUSD;
+                break;
+
+            case "GBP":
+                result = amount / rateGBP;
+                break;
+        }
+
+
+
+
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+    });
+    welcome();
+
+
+}
